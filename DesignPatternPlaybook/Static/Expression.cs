d97a474fb05a7d7b65901b50716969c79c96681c
Roel <roelverschuijten@gmail.com>
2019-11-05 16:20:57 +0100

    added func as parameter

---
+++ DesignPatternPlaybook/Static/Expression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DesignPatternPlaybook.Static
+{
+    public class Expression
+    {
+        private static Expression<Func<int, int>> square = x => x * x;
+        
+        public static void MainFunc()
+        {
+            Console.WriteLine("Func takes an input and returns the OUTPUT. The last parameter.");
+
+            
+
+        }
+    }
+}
--- DesignPatternPlaybook/Static/Func.cs
+++ DesignPatternPlaybook/Static/Func.cs
@@ -7,27 +7,32 @@ namespace DesignPatternPlaybook.Static
     public static class Funcs
     {
 
+        private static Func<int, int> funcsquare = x => x*x;
+
         private static Func<int, string> IntToString = x => x.ToString();
 
         private static Func<string, bool> CompareString(string q)
         {
             return x => (q == x );
         }
+
         private static Func<CustomClass, bool> CompareClassString(string q)
         {
             return x => (q == x.Name) || (q == x.Description);
         }
 
-
-
         public static void MainFunc()
         {
             Console.WriteLine("Func takes an input and returns the OUTPUT. The last parameter.");
+            
+            Console.WriteLine("Func as a parameter.");
+            WithFunc(funcsquare);
+
 
             Func<int, int> square = x => x*x;
             Func<int, int,int> add = (x,y) => x+y;
 
-
+            Console.WriteLine("Plain func result");
             Console.WriteLine(square(3));
             Console.WriteLine(add(3,8));
             Console.WriteLine(IntToString(3));
@@ -54,10 +59,19 @@ namespace DesignPatternPlaybook.Static
         }
 
 
+        public static void WithFunc(Func<int, int> square1)
+        {
+            int it = square1(3);
+            Console.WriteLine(it);
 
+        }
     }
 
-    internal class CustomClass
+
+
+
+
+    public class CustomClass
     {
         public string Name {get;set;}
         public string Description {get;set;}
