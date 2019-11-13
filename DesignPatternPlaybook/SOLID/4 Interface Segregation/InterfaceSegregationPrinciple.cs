90554571cf3b0b224d821565d977dc0be447302f
Roel <roelverschuijten@gmail.com>
2019-11-13 14:22:01 +0100

    Update InterfaceSegregationPrinciple.cs

--- DesignPatternPlaybook/SOLID/4 Interface Segregation/InterfaceSegregationPrinciple.cs
+++ DesignPatternPlaybook/SOLID/4 Interface Segregation/InterfaceSegregationPrinciple.cs
@@ -1,13 +1,27 @@
-﻿namespace DesignPatternPlaybook.SOLID._4_Interface_Segregation
+//Violation
+//In programming, the interface segregation principle states that no client should be forced to depend on methods it does not use.
+//Put more simply: Do not add additional functionality to an existing interface by adding new methods.
+//Instead, create a new interface and let your class implement multiple interfaces if needed.
+interface IPost
 {
-    public class InterfaceSegregationPrinciple
-    {
-        // Define
-        // Clients should not depend on methods they do not use
+    void CreatePost();
+}
+
+interface IPostNew
+{
+    void CreatePost();
+    void ReadPost();
+}
 
 
-        
-    }
+//Solved
+//If any class might need both the CreatePost() method and the ReadPost() method, it will implement both interfaces.
+interface IPostCreate
+{
+    void CreatePost();
+}
 
-
+interface IPostRead
+{
+    void ReadPost();
 }
